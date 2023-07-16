a63289067d4a01a0d43443d293121f4768a79554
Alexis Ramos <alexleaderhere@gmail.com>
2023-07-16 00:10:03 -0300

    changes

--- veterinaria-mi-mascota/src/components/NavBar.vue
+++ veterinaria-mi-mascota/src/components/NavBar.vue
@@ -53,7 +53,7 @@ export default {
 methods: {
   
   getUserData() {
-    if (this.isLoggedIn) {
+   
     fetch('/user-data/')
       .then(response => response.json())
       .then(data => {
@@ -62,7 +62,7 @@ methods: {
       .catch(error => {
       console.error('Error al obtener los datos del usuario:', error);
     });
-  }
+
   },
   logout() {
     const csrftoken = this.getCookie('csrftoken');
